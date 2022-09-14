29ed0fcfd2c11509cd317cdd151aef1128792d7d
Smunfr <christian.schlecht97@web.de>
2022-09-14 11:20:35 +0200

    on application startup build text indexes needed for full text search

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ if sys.platform == 'win32':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 from keycloak import KeycloakOpenID
+import pymongo
 import tornado.httpserver
 import tornado.ioloop
 import tornado.locks
@@ -31,6 +32,8 @@ define("no_wiki", default=False, type=bool,
        help="start without wiki integration (use if u don't have the wiki software installed and running)")
 define("config", default="config.json", type=str,
        help="path to config file, defaults to config.json")
+define("build_text_index", default=False, type=bool,
+       help="force the application to (re)build the text index for full text search")
 
 
 class RoutingHandler(BaseHandler):
@@ -84,6 +87,44 @@ def make_app(cookie_secret):
     ], cookie_secret=cookie_secret, template_path="html")
 
 
+def init_text_indexes(force_rebuild: bool) -> None:
+    """
+    build the text indexes for posts and profiles.
+    the weights of the fields are left default (1).
+    indexes will be build if a) they don't exist or b) if rebuild is forced by setting force_rebuild to True)
+    
+    :param force_rebuild: boolean switch to trigger a forced rebuild of the text indexes
+    """
+    
+    client = pymongo.MongoClient(global_vars.mongodb_host, global_vars.mongodb_port,
+                              username=global_vars.mongodb_username, password=global_vars.mongodb_password)
+    db = client[global_vars.mongodb_db_name]
+
+    # only build the index if they are either not present or we forced a rebuild
+    if "posts" not in db.posts.index_information() or force_rebuild:
+        db.posts.drop_index("posts")
+        db.posts.create_index([("text", pymongo.TEXT),
+                               ("tags", pymongo.TEXT)],
+                               name="posts")
+        print("Built text index named {} on collection {}".format("posts", "posts"))
+
+    if "profiles" not in db.profiles.index_information() or force_rebuild:
+        db.profiles.drop_index("profiles")
+        db.profiles.create_index([("bio", pymongo.TEXT),
+                                  ("institution", pymongo.TEXT),
+                                  ("projects", pymongo.TEXT),
+                                  ("first_name", pymongo.TEXT),
+                                  ("last_name", pymongo.TEXT),
+                                  ("gender", pymongo.TEXT),
+                                  ("address", pymongo.TEXT),
+                                  ("birthday", pymongo.TEXT),
+                                  ("experience", pymongo.TEXT),
+                                  ("education", pymongo.TEXT),
+                                  ("user", pymongo.TEXT)],
+                                  name="profiles")   
+        print("Built text index named {} on collection {}".format("profiles", "profiles"))
+
+
 async def main():
     signing.create_signing_key_if_not_exists()
 
@@ -115,12 +156,17 @@ async def main():
     global_vars.keycloak = KeycloakOpenID(conf["keycloak_base_url"], realm_name=conf["keycloak_realm"],
                                           client_id=conf["keycloak_client_id"], client_secret_key=conf["keycloak_client_secret"])
 
+    # build and start server
     cookie_secret = conf["cookie_secret"]
     app = make_app(cookie_secret)
     server = tornado.httpserver.HTTPServer(app)
     print("Starting server on port: " + str(global_vars.port))
     server.listen(global_vars.port)
 
+    # setup text indexes for searching
+    init_text_indexes(options.build_text_index)
+
+    # register with the platform
     client = await get_socket_instance()
     response = await client.write({"type": "module_start",
                                    "module_name": "lionet",
