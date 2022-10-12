49f825ea29e71fd346fa9b68d4ca6be26dd39252
Smunfr <christian.schlecht97@web.de>
2022-10-12 15:37:10 +0200

    add test for follow handler

--- tests/api_tests.py
+++ tests/api_tests.py
@@ -1,13 +1,18 @@
 import json
 
 from keycloak import KeycloakAdmin, KeycloakOpenID
+from pymongo import MongoClient
 from tornado.options import options
-from tornado.testing import AsyncHTTPTestCase, gen_test
+from tornado.testing import AsyncHTTPTestCase
 
 import global_vars
 from main import make_app
+from model import User
 
 MISSING_KEY_ERROR_SLUG = "missing_key:"
+
+CURRENT_USER = User(
+    "test_admin", "aaaaaaaa-bbbb-0000-cccc-dddddddddddd", "test_admin@mail.de") # don't change, these values match with the ones in BaseHandler
 
 def setup():
     # deal with config properties
@@ -27,7 +32,7 @@ def setup():
     global_vars.mongodb_port = config["mongodb_port"]
     global_vars.mongodb_username = config["mongodb_username"]
     global_vars.mongodb_password = config["mongodb_password"]
-    global_vars.mongodb_db_name = config["mongodb_db_name"]
+    global_vars.mongodb_db_name = "test_db"
 
     # set test mode to bypass authentication
     options.test = True
@@ -44,7 +49,7 @@ def validate_json_str(suspect_str: str) -> bool:
 class RenderHandlerTest(AsyncHTTPTestCase):
 
     def get_app(self):
-        setup() # have to do our setup here, because get_app and setUp seemingly have a race condition, but we need to make sure cookie_secret is set before make_app
+        setup() # have to do our setup here, because parent class calls get_app in its own setUp, so we would have to setup before super() otherwise, which might break something idk
         return make_app(global_vars.cookie_secret)
 
     def setUp(self) -> None:
@@ -85,12 +90,58 @@ class RenderHandlerTest(AsyncHTTPTestCase):
                 self.fetch_and_assert_is_html(endpoint)
 
 
-class AuthenticationHandlersTest(AsyncHTTPTestCase):
-    # the only authentication we can really effectively test is the redirect to keycloak and the error case for the callback...
+class BaseApiTestCase(AsyncHTTPTestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+
+        # initialize mongodb connection
+        self.client = MongoClient(global_vars.mongodb_host, global_vars.mongodb_port,
+                                  username=global_vars.mongodb_username, password=global_vars.mongodb_password)
+        self.db = self.client[global_vars.mongodb_db_name]
+
+    def tearDown(self) -> None:
+        # close mongodb connection
+        self.client.close()
+        super().tearDown()
 
     def get_app(self):
-        setup()  # have to do our setup here, because get_app and setUp seemingly have a race condition, but we need to make sure cookie_secret is set before make_app
+        setup()  # have to do our setup here, because parent class calls get_app in its own setUp, so we would have to setup before super() otherwise, which might break something idk
         return make_app(global_vars.cookie_secret)
+
+    def base_checks(self, method: str, url: str, expect_success: bool, expect_response_code: str) -> dict:
+        """
+        convenience wrapper to assert the following:
+        - response matches expected http code
+        - response contains valid json
+        - response json contains a "success" key
+        - "success" matches expected success value
+        
+        :returns: response content
+        """
+
+        response = self.fetch(url, method=method)
+        content = response.buffer.getvalue().decode()
+
+        # match expected response code
+        self.assertEqual(response.code, expect_response_code)
+
+        # expect valid json as response content
+        self.assertIsInstance(content, str)
+        is_json = validate_json_str(content)
+        self.assertEqual(is_json, True)
+
+        content = json.loads(content)
+
+        # expect a "success" key and match expected value
+        self.assertIn("success", content)
+        self.assertEqual(content["success"], expect_success)
+
+        return content
+
+
+class AuthenticationHandlersTest(BaseApiTestCase):
+    # the only authentication we can really effectively test is the redirect to keycloak and the error case for the callback...
 
     def test_login_redirect(self):
         """
@@ -105,25 +156,42 @@ class AuthenticationHandlersTest(AsyncHTTPTestCase):
         expect: 400 Bad request, reason missing_key
         """
 
-        response = self.fetch("/login/callback")
-        content = response.buffer.getvalue().decode()
-
-        # expect response code 400
-        self.assertEqual(response.code, 400)
-
-        # expect valid json as response content
-        self.assertIsInstance(content, str)
-        is_json = validate_json_str(content)
-        self.assertEqual(is_json, True)
-
-        content = json.loads(content)
-
-        # expect a "success" key and that it has False value
-        self.assertIn("success", content)
-        self.assertEqual(content["success"], False)
+        response = self.base_checks("GET", "/login/callback", False, 400)
 
         # expect a missing_key:code error as the reason
-        self.assertIn("reason", content)
-        self.assertEqual(content["reason"], MISSING_KEY_ERROR_SLUG + "code")
-        
+        self.assertIn("reason", response)
+        self.assertEqual(response["reason"], MISSING_KEY_ERROR_SLUG + "code")
 
+
+class FollowHandlerTest(BaseApiTestCase):
+
+    def setUp(self) -> None:
+        super().setUp()
+
+        self.user_follows = ["test_user1", "test_user2"]
+        
+        # insert test data
+        self.db.follows.insert_one({
+            "user": CURRENT_USER.username,
+            "follows": self.user_follows
+        })
+
+    def test_get_follows(self):
+        """
+        expect: a dict containing the above set-up follow-relation
+        """
+
+        response = self.base_checks("GET", "/follow?user={}".format(CURRENT_USER.username), True, 200)
+
+        # expect a users and a follows key
+        self.assertIn("user", response)
+        self.assertIn("follows", response)
+
+        # expect user to be the requested one and the users he follows as stated in the setup
+        self.assertEqual(response["user"], CURRENT_USER.username)
+        self.assertEqual(response["follows"], self.user_follows)
+
+    def tearDown(self) -> None:
+        # cleanup test data
+        self.db.follows.delete_many({})
+        super().tearDown()
