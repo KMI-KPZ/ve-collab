7f8d4c53d5d9d1e87c21a97cfaf1b60894eb4b3c
Teroxin97 <teroxin1997@gmail.com>
2022-11-02 10:59:05 +0100

    Added space reference in post header, readded post button to comments, reworked repost modal and added functionality to delete items from pending posts

--- javascripts/newsfeed.js
+++ javascripts/newsfeed.js
@@ -302,9 +302,15 @@ $body.delegate('#files', 'change', function () {
   for(var i=0; i < fileInput.files.length; i++) {
     console.log(fileInput.files[i]);
     fileList.push(fileInput.files[i]);
-    $('#postdiv').append('<span class="name">'+fileInput.files[i].name+'</span></br>');
+    $('#postdiv').append('<div><span id="trash_item">'+fileInput.files[i].name+'<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-2 inline-block"><path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" /></svg></span></div></br>');
   }
 });
+
+$body.delegate('#trash_item', 'click', function(e) {
+  e.currentTarget.parentNode.remove()
+  var fileName = e.currentTarget.innerText
+  var fileList = fileList.filter(file => file.name != fileName);
+}) 
 
 /**
  * getExtension - get extension of a filename
