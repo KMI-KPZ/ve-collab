332e36442eeeba3fe79d866da3a44fee985e96f2
JonasGreim <jonasgreim@gmail.com>
2023-04-26 11:43:15 +0200

    remove context plaid

--- frontend-vecollab/src/pages/_app.tsx
+++ frontend-vecollab/src/pages/_app.tsx
@@ -1,33 +1,21 @@
-import React, { createContext, useState } from 'react';
+import React from 'react';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import LayoutSection from '@/components/Layout/LayoutSection';
 import Head from 'next/head';
-import { SessionProvider } from "next-auth/react"
-
-interface PlanId {
-    planId: string,
-    setPlanId: Function
-}
-
-export const PlanIdContext = React.createContext<PlanId>({planId: "", setPlanId: function(){}})
+import { SessionProvider } from 'next-auth/react';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-
-    const [planId, setPlanId] = useState("")
-
     return (
         <>
-            <PlanIdContext.Provider value={{planId, setPlanId}}>
-                <SessionProvider session={session}>
-                    <Head>
-                        <title>Ve Collab</title>
-                    </Head>
-                    <LayoutSection>
-                        <Component {...pageProps} />
-                    </LayoutSection>
-                </SessionProvider>
-            </PlanIdContext.Provider>
+            <SessionProvider session={session}>
+                <Head>
+                    <title>Ve Collab</title>
+                </Head>
+                <LayoutSection>
+                    <Component {...pageProps} />
+                </LayoutSection>
+            </SessionProvider>
         </>
     );
 }
