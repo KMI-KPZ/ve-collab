10d98b942e4603a4bec74654b6c03b3c422faff0
Jonas Greim <jonas.greim@wtl.de>
2023-05-22 13:43:13 +0200

    add react hook form: New content question (#38)
    * add meta tags to show favicon and link preview
    
    * add react form hook without error box
    
    * add error message

--- frontend-vecollab/src/pages/_app.tsx
+++ frontend-vecollab/src/pages/_app.tsx
@@ -4,6 +4,8 @@ import type { AppProps } from 'next/app';
 import LayoutSection from '@/components/Layout/LayoutSection';
 import Head from 'next/head';
 import { SessionProvider } from 'next-auth/react';
+import Favicon from '@/components/metaTags/Favicon';
+import LinkPreview from '@/components/metaTags/LinkPreview';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
@@ -11,6 +13,8 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
             <SessionProvider session={session}>
                 <Head>
                     <title>Ve Collab</title>
+                    <Favicon />
+                    <LinkPreview />
                 </Head>
                 <LayoutSection>
                     <Component {...pageProps} />
