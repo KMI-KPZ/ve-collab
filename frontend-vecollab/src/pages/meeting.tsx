2e8679dfab134f74865adfc0c594a2fd1c9de745
Smunfr <christian.schlecht97@web.de>
2023-07-05 11:14:07 +0200

    protect meeting page by valid user session

--- frontend-vecollab/src/pages/meeting.tsx
+++ frontend-vecollab/src/pages/meeting.tsx
@@ -1,11 +1,23 @@
 import LoadingAnimation from '@/components/LoadingAnimation';
 import { JitsiMeeting } from '@jitsi/react-sdk';
+import { signIn, useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 export default function Meeting() {
+    const { data: session, status } = useSession();
     const router = useRouter();
     const [loading, setLoading] = useState(true);
+
+    // check for session errors and trigger the login flow if necessary
+    useEffect(() => {
+        if (status !== 'loading') {
+            if (!session || session?.error === 'RefreshAccessTokenError') {
+                console.log('forced new signIn');
+                signIn('keycloak');
+            }
+        }
+    }, [session, status]);
 
     // we need to wait for the router to be ready to initialize the jitsi meeting,
     // otherwise router.query.meetingId would be undefined, cause Jitsi to crash
