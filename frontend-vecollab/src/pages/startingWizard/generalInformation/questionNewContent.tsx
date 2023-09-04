f10ca180367c0771f06746e6128ec4285965dc0f
JonasGreim <jonasgreim@gmail.com>
2023-09-04 09:22:06 +0200

    add sideMenu

--- frontend-vecollab/src/pages/startingWizard/generalInformation/questionNewContent.tsx
+++ frontend-vecollab/src/pages/startingWizard/generalInformation/questionNewContent.tsx
@@ -13,6 +13,7 @@ import {
     ProgressState,
 } from '@/interfaces/startingWizard/sideProgressBar';
 import { useValidation } from '@/components/StartingWizard/ValidateRouteHook';
+import { sideMenuStepsData } from '@/data/sideMenuSteps';
 
 interface FormValues {
     newContent: string;
@@ -192,6 +193,7 @@ export default function NewContent() {
                     progressState={sideMenuStepsProgress}
                     handleValidation={handleSubmit(onSubmit)}
                     isValid={isValid}
+                    sideMenuStepsData={sideMenuStepsData}
                 />
             </div>
         </>
