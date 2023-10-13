12fd6dc31c9d59be5d9a72c80587f39972d80067
Smunfr <christian.schlecht97@web.de>
2023-10-13 08:59:53 +0200

    fix missing env variables when frontend has no separate .env file and should inherit them from docker compose

--- frontend-vecollab/src/lib/socket.ts
+++ frontend-vecollab/src/lib/socket.ts
@@ -1,3 +1,7 @@
 import { io } from 'socket.io-client';
 
+// TODO need "path" option to be able to connect to socket.io server if the backend
+// is running behind a nginx location block (e.g. /backend/socket.io/ needs "path": "/backend/socket.io")
+// OR configure nginx to proxy pass /backend/socket.io directly to the backends /socket.io
+// with a custom nginx /socket.io location
 export const socket = io(process.env.NEXT_PUBLIC_BACKEND_BASE_URL!, { autoConnect: false });
