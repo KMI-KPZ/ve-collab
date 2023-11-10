557ab2b47c0cf9e56bac583a5ae56fe77043a763
JonasGreim <jonasgreim@gmail.com>
2023-11-10 15:49:32 +0100

    make components: ViewAttributes, ViewFinestep

---
+++ frontend-vecollab/src/components/planSummary/ViewFinestep.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { showDataOrEmptySign } from '@/pages/planSummary/[planSummarySlug]';
+import { IFineStep } from '@/pages/startingWizard/fineplanner/[stepSlug]';
+
+interface Props {
+    fineStep: IFineStep;
+}
+
+export default function ViewFinestep({ fineStep }: Props): JSX.Element {
+    const convertDateToLocal = (timestamp: string) => {
+        return new Date(timestamp).toLocaleString('de-DE', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+        });
+    };
+    return (
+        <div className="p-4 my-8 bg-white border-2 border-gray-400 rounded-3xl">
+            <div className="flex justify-center items-center space-x-10">
+                <div className="flex">
+                    <div className="font-bold text-xl mx-2">Etappe:</div>
+                    <div className="font-bold text-xl">{showDataOrEmptySign(fineStep.name)}</div>
+                </div>
+                <div className="flex">
+                    <div className="font-bold mx-2">Zeitspanne:</div>
+                    <div className="mx-2">
+                        {showDataOrEmptySign(convertDateToLocal(fineStep.timestamp_from))}
+                        {' - '}
+                        {showDataOrEmptySign(convertDateToLocal(fineStep.timestamp_to))}
+                    </div>
+                </div>
+            </div>
+            <div className="mt-4 flex">
+                <div className="w-1/6 flex items-center">
+                    <p className="font-semibold px-2 py-2">Workload</p>
+                </div>
+                <div className="flex items-center w-fit bg-slate-200 rounded-lg px-3">
+                    {showDataOrEmptySign(fineStep.workload) + ' Stunden'}
+                </div>
+            </div>
+            <div className="mt-4 flex">
+                <div className="w-1/6 flex items-center">
+                    <p className="font-semibold px-2 py-2 flex items-center">Sozialform</p>
+                </div>
+                <div className="flex items-center w-fit bg-slate-200 rounded-lg px-3">
+                    {showDataOrEmptySign(fineStep.social_form)}
+                </div>
+            </div>
+            <div className="mt-4 flex">
+                <div className="w-1/6 flex items-center">
+                    <p className="font-semibold px-2 py-2 ">digitale Lernumgebung</p>
+                </div>
+                <div className="flex items-center w-fit bg-slate-200 rounded-lg px-3">
+                    {showDataOrEmptySign(fineStep.learning_env)}
+                </div>
+            </div>
+            <div className="mt-4 flex">
+                <div className="w-1/6 flex items-center">
+                    <p className="font-semibold px-2 py-2">VE-Ansatz</p>
+                </div>
+                <div className="flex items-center w-fit bg-slate-200 rounded-lg px-3">
+                    {showDataOrEmptySign(fineStep.ve_approach)}
+                </div>
+            </div>
+            <div className="mt-4 flex">
+                <div className="font-semibold w-1/5 flex items-center px-2 py-2px-2 py-2">
+                    Aufgabenstellungen
+                </div>
+                {fineStep.tasks.map((task, taskIndex) => (
+                    <div
+                        className="flex flex-col space-y-1 w-1/2 p-4 my-4 mx-2 bg-slate-200 rounded-3xl shadow-2xl "
+                        key={taskIndex}
+                    >
+                        <div className="flex space-x-8">
+                            <span className="w-1/4 font-medium">Titel:</span>
+                            <span>{showDataOrEmptySign(task.title)}</span>
+                        </div>
+                        <div className="flex space-x-8">
+                            <span className="w-1/4 font-medium">Beschreibung:</span>
+                            <span>{showDataOrEmptySign(task.description)}</span>
+                        </div>
+                        <div className="flex space-x-8">
+                            <span className="w-1/4 font-medium">Lernziele:</span>
+                            <span>{showDataOrEmptySign(task.learning_goal)}</span>
+                        </div>
+                        <div className="flex space-x-8">
+                            <span className="w-1/4 font-medium">Beschreibung:</span>
+                            <span>{showDataOrEmptySign(task.description)}</span>
+                        </div>
+                        <div className="flex space-x-8">
+                            <span className="w-1/4 font-medium">Lernziel:</span>
+                            <span>{showDataOrEmptySign(task.learning_goal)}</span>
+                        </div>
+                        <div className="flex space-x-8">
+                            <span className="w-1/4 font-medium">Tools:</span>
+                            <span>
+                                {showDataOrEmptySign(
+                                    task.tools.filter((element) => element !== '').join(', ')
+                                )}
+                            </span>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
