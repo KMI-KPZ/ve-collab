9b47e6053717ee60c714acfcceca3b986579db59
Smunfr <christian.schlecht97@web.de>
2023-11-23 10:49:29 +0100

    i cant do this anymore, ill just leave the socketio tests be...

--- backend/run_tests.py
+++ backend/run_tests.py
@@ -4,11 +4,8 @@ from tornado.options import define
 import tornado.testing
 
 
-# socketio tests need to run before any other tests that need an io loop, otherwise they crash
-# why on earth tho...
 TEST_MODULES = [
     "tests.model_tests",
-    "tests.socketio_tests",
     "tests.resource_tests",
     "tests.api_tests",
 ]
