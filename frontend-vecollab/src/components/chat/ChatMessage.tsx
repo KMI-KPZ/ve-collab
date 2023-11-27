0e75218ca50a15009404b499e84f7c9b48bc80bf
Smunfr <christian.schlecht97@web.de>
2023-11-27 14:51:00 +0100

    fix #114, leave whitespaces and newlines in chat messages

--- frontend-vecollab/src/components/chat/ChatMessage.tsx
+++ frontend-vecollab/src/components/chat/ChatMessage.tsx
@@ -8,13 +8,13 @@ export default function ChatMessage({ message, currentUser }: Props) {
         <li>
             {message.sender === currentUser ? (
                 <div className="flex justify-end">
-                    <div className="bg-ve-collab-blue/50 rounded-md p-2 m-1 max-w-4xl break-words">
+                    <div className="bg-ve-collab-blue/50 rounded-md p-2 m-1 max-w-4xl break-words whitespace-pre-wrap">
                         <p>{message.message}</p>
                     </div>
                 </div>
             ) : (
                 <div className="flex flex-col items-start">
-                    <div className="bg-gray-200 rounded-md p-2 m-1 max-w-4xl break-words">
+                    <div className="bg-gray-200 rounded-md p-2 m-1 max-w-4xl break-words whitespace-pre-wrap">
                         <p>{message.message}</p>
                     </div>
                 </div>
