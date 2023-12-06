cf059696c1691d14ecc857572dbe304cd3a47e31
Smunfr <christian.schlecht97@web.de>
2023-12-06 15:26:32 +0100

    implement members view of space

--- frontend-vecollab/src/pages/space.tsx
+++ frontend-vecollab/src/pages/space.tsx
@@ -1,12 +1,15 @@
+import AuthenticatedImage from '@/components/AuthenticatedImage';
 import BoxHeadline from '@/components/BoxHeadline';
 import WhiteBox from '@/components/Layout/WhiteBox';
 import Container from '@/components/Layout/container';
 import GroupBanner from '@/components/network/GroupBanner';
 import GroupHeader from '@/components/network/GroupHeader';
-import { useGetSpace } from '@/lib/backend';
+import { UserSnippet } from '@/interfaces/profile/profileInterfaces';
+import { fetchPOST, useGetSpace } from '@/lib/backend';
+import { is } from 'date-fns/locale';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 Space.auth = true;
 export default function Space() {
@@ -15,6 +18,10 @@ export default function Space() {
 
     const [renderPicker, setRenderPicker] = useState<'timeline' | 'members' | 'files'>('timeline');
 
+    const [memberSnippets, setMemberSnippets] = useState<UserSnippet[]>([
+        { name: '', profilePicUrl: '', institution: '', preferredUsername: '' },
+    ]);
+
     const {
         data: space,
         isLoading,
@@ -22,6 +29,106 @@ export default function Space() {
         mutate,
     } = useGetSpace(session!.accessToken, router.query.name as string);
     console.log(space);
+    console.log(memberSnippets);
+
+    useEffect(() => {
+        if (isLoading) {
+            return;
+        }
+        fetchPOST('/profile_snippets', { usernames: space.members }, session?.accessToken).then(
+            (data) => {
+                setMemberSnippets(
+                    data.user_snippets.map((snippet: any) => ({
+                        name: snippet.first_name + ' ' + snippet.last_name,
+                        profilePicUrl: snippet.profile_pic,
+                        institution: snippet.institution,
+                        preferredUsername: snippet.username,
+                    }))
+                );
+            }
+        );
+    }, [space, isLoading]);
+
+    function files() {
+        return <div>Dateiablage</div>;
+    }
+
+    function members() {
+        return (
+            <WhiteBox>
+                <BoxHeadline title={'Admins'} />
+                <hr className="h-px mt-2 mb-6 bg-gray-200 border-0" />
+                <div className="mb-8 flex flex-wrap max-h-72 overflow-y-auto content-scrollbar">
+                    {space.admins.map((admin, index) => (
+                        <div
+                            key={index}
+                            className="mx-6 my-1 cursor-pointer"
+                            onClick={() => router.push(`/profile?username=${admin}`)}
+                        >
+                            <div className="flex justify-center">
+                                <AuthenticatedImage
+                                    imageId={
+                                        memberSnippets.find(
+                                            (snippet) => snippet.preferredUsername === admin
+                                        )!.profilePicUrl
+                                    }
+                                    alt={'Profilbild'}
+                                    width={100}
+                                    height={100}
+                                    className="rounded-full"
+                                ></AuthenticatedImage>
+                            </div>
+                            <BoxHeadline
+                                title={
+                                    memberSnippets.find(
+                                        (snippet) => snippet.preferredUsername === admin
+                                    )!.name
+                                }
+                            />
+                        </div>
+                    ))}
+                </div>
+                <BoxHeadline title={'Mitglieder'} />
+                <hr className="h-px mt-2 mb-6 bg-gray-200 border-0" />
+                <div className="flex flex-wrap max-h-72 overflow-y-auto content-scrollbar">
+                    {space.members
+                        .filter((member) => !space.admins.includes(member))
+                        .map((member, index) => (
+                            <div
+                                key={index}
+                                className="mx-6 my-1 cursor-pointer"
+                                onClick={() => router.push(`/profile?username=${member}`)}
+                            >
+                                <div className="flex justify-center">
+                                    <AuthenticatedImage
+                                        imageId={
+                                            memberSnippets.find(
+                                                (snippet) => snippet.preferredUsername === member
+                                            )!.profilePicUrl
+                                        }
+                                        alt={'Profilbild'}
+                                        width={100}
+                                        height={100}
+                                        className="rounded-full"
+                                    ></AuthenticatedImage>
+                                </div>
+                                <BoxHeadline
+                                    title={
+                                        memberSnippets.find(
+                                            (snippet) => snippet.preferredUsername === member
+                                        )!.name
+                                    }
+                                />
+                            </div>
+                        ))}
+                </div>
+            </WhiteBox>
+        );
+    }
+
+    function timeline() {
+        return <div>Timeline</div>;
+    }
 
     return (
         <Container>
@@ -35,14 +142,14 @@ export default function Space() {
                         {(() => {
                             switch (renderPicker) {
                                 case 'timeline':
-                                    return <div>Timeline</div>;
+                                    return timeline();
                                 case 'members':
-                                    return <div>Mitglieder</div>;
+                                    return members();
                                 case 'files':
-                                    return <div>Dateiablage</div>;
+                                    return files();
 
                                 default:
-                                    return <div>dont know</div>;
+                                    return <div></div>;
                             }
                         })()}
                     </div>
