a7e56b5676eb35d76c7c0afbe65e2709a1f81841
Simeon Ackermann <s.ackermann@mail.de>
2024-02-14 12:09:45 +0100

    Add new post form

--- frontend-vecollab/src/components/network/Timeline.tsx
+++ frontend-vecollab/src/components/network/Timeline.tsx
@@ -1,9 +1,10 @@
-import { useGetTimeline } from "@/lib/backend";
+import { fetchPOST, fetchPOSTAsFormdata, useGetTimeline } from "@/lib/backend";
 import { useSession } from "next-auth/react";
 import LoadingAnimation from "../LoadingAnimation";
 import TimelinePost from "./TimelinePost";
-import { useState } from "react";
-import { access } from "fs";
+import { FormEvent, useState } from "react";
+import { IoIosSend } from "react-icons/io";
+import AuthenticatedImage from "../AuthenticatedImage";
 
 interface Props {
     space?: string | undefined;
@@ -12,7 +13,7 @@ interface Props {
 Timeline.auth = true
 export default function Timeline({ space }: Props) {
     const { data: session } = useSession();
-    const [toDate] = useState(new Date().toISOString());
+    const [toDate, setToDate] = useState(new Date().toISOString());
 
     const now = new Date()
     const [fromDate] = useState( new Date(now.setFullYear( now.getFullYear() - 1 )).toISOString());
@@ -20,7 +21,7 @@ export default function Timeline({ space }: Props) {
 
     const {
         data: timeline,
-        isLoading,
+        isLoading: isLoadingTimeline,
         error,
         mutate,
     } = useGetTimeline(
@@ -32,10 +33,39 @@ export default function Timeline({ space }: Props) {
     )
     console.log({timeline, space});
 
-    // TODO new post form
     // TODO infinite scroll
+    // TODO user profile pic should be part of session?.user
 
-    if (isLoading) {
+    const onSubmitForm = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget)
+        const text = (formData.get('text') as string).trim()
+
+        if (text === '')  return
+
+        // TODO currently we reload the whole timeline
+        //  to only load the new post the endpoint "/posts" may should return the newly added post which
+        //  we can append here
+        //  see https://blog.logrocket.com/handling-data-fetching-next-js-useswr/#mutation-revalidation
+        //  alternative we load posts fromDate (last toDate) toDate (new now) and append
+
+        try {
+            await fetchPOST(
+                '/posts',
+                {
+                    text,
+                    tags: []
+                },
+                session?.accessToken,
+                true
+            )
+            setToDate(new Date().toISOString())
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    if (isLoadingTimeline) {
         return (<><LoadingAnimation /></>)
     }
 
@@ -46,6 +76,32 @@ export default function Timeline({ space }: Props) {
 
     return (
         <>
+            <div className={'p-4 my-8 bg-white rounded-3xl shadow-2xl '}>
+                <form onSubmit={onSubmitForm}>
+                    <div className="flex items-center mb-5">
+                        <AuthenticatedImage
+                            imageId={"default_profile_pic.jpg"}
+                            alt={'Benutzerbild'}
+                            width={40}
+                            height={40}
+                            className="rounded-full mr-3"
+                        ></AuthenticatedImage>
+                        <textarea
+                            className={'w-full border border-[#cccccc] rounded-md px-2 py-[6px]'}
+                            placeholder={'Beitrag schreiben ...'}
+                            name='text'
+                        />
+                    </div>
+                    <div className="flex justify-end">
+
+
+
+                    <button className="flex items-center bg-ve-collab-orange text-white py-2 px-5 rounded-lg" type='submit' title="Senden">
+                        <IoIosSend className="mx-2" />Senden
+                    </button>
+                    </div>
+                </form>
+            </div>
             {!timeline.length ? ( <div>Timeline is Empty</div>) : (<></>)}
             {timeline.map((post, i) =>
                 <TimelinePost key={i} post={post} mutate={mutate} />
