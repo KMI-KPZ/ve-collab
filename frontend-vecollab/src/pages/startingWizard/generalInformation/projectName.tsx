b361a5b01db7ba94a0e2877ec0ea27f8e0d0f87e
JonasGreim <jonasgreim@gmail.com>
2024-03-05 11:26:54 +0100

    projectName change back

--- frontend-vecollab/src/pages/startingWizard/generalInformation/projectName.tsx
+++ frontend-vecollab/src/pages/startingWizard/generalInformation/projectName.tsx
@@ -12,12 +12,12 @@ import {
 } from '@/interfaces/startingWizard/sideProgressBar';
 import { sideMenuStepsData } from '@/data/sideMenuSteps';
 import { IFineStep } from '@/pages/startingWizard/fineplanner/[stepSlug]';
-import { z } from 'zod';
+import { useValidation } from '@/components/StartingWizard/ValidateRouteHook';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-const vaidationSchema = z
-    .string()
-    .max(40, { message: 'Der Name darf maximal 40 Zeichen lang sein' })
-    .min(1, { message: 'Bitte gib dem Plan einen Namen' });
+interface FormData {
+    name: string;
+}
 
 export default function EssentialInformation() {
     const { data: session, status } = useSession();
@@ -27,8 +27,7 @@ export default function EssentialInformation() {
         initialSideProgressBarStates
     );
     const [steps, setSteps] = useState<IFineStep[]>([]);
-    const [planName, setPlanName] = useState<string>('');
-    const [errors, setErrors] = useState<string[]>();
+    const { validateAndRoute } = useValidation();
 
     // check for session errors and trigger the login flow if necessary
     useEffect(() => {
@@ -39,6 +38,13 @@ export default function EssentialInformation() {
             }
         }
     }, [session, status]);
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isValid },
+        setValue,
+    } = useForm<FormData>({ mode: 'onChange' });
 
     useEffect(() => {
         // if router or session is not yet ready, don't make an redirect decisions or requests, just wait for the next re-render
@@ -60,47 +66,34 @@ export default function EssentialInformation() {
                         setSideMenuStepsProgress(data.plan.progress);
                     }
                     setSteps(data.plan.steps);
-                    setPlanName(data.plan.name);
+                    setValue('name', data.plan.name, { shouldValidate: true });
                 }
             );
         }
-    }, [session, status, router]);
+    }, [session, status, router, setValue]);
 
-    const handleSubmit = async (e: React.MouseEvent, routePath: string) => {
-        e.preventDefault();
-        setLoading(true);
-        const validated = vaidationSchema.safeParse(planName);
-        if (validated.success) {
-            await fetchPOST(
-                '/planner/update_fields',
-                {
-                    update: [
-                        {
-                            plan_id: router.query.plannerId,
-                            field_name: 'name',
-                            value: planName,
+    const onSubmit: SubmitHandler<FormData> = async (data: FormData) => {
+        await fetchPOST(
+            '/planner/update_fields',
+            {
+                update: [
+                    {
+                        plan_id: router.query.plannerId,
+                        field_name: 'name',
+                        value: data.name,
+                    },
+                    {
+                        plan_id: router.query.plannerId,
+                        field_name: 'progress',
+                        value: {
+                            ...sideMenuStepsProgress,
+                            name: ProgressState.completed,
                         },
-                        {
-                            plan_id: router.query.plannerId,
-                            field_name: 'progress',
-                            value: {
-                                ...sideMenuStepsProgress,
-                                name: ProgressState.completed,
-                            },
-                        },
-                    ],
-                },
-                session?.accessToken
-            );
-            await router.push({
-                pathname: routePath,
-                query: { plannerId: router.query.plannerId },
-            });
-        } else {
-            const formatted = validated.error.format();
-            setErrors(formatted._errors);
-        }
-        setLoading(false);
+                    },
+                ],
+            },
+            session?.accessToken
+        );
     };
 
     return (
@@ -121,42 +114,59 @@ export default function EssentialInformation() {
                                         type="text"
                                         placeholder="Name eingeben"
                                         className="border border-gray-500 rounded-lg w-3/4 h-12 p-2"
-                                        value={planName}
-                                        onChange={(e) => setPlanName(e.target.value)}
+                                        {...register('name', {
+                                            required: {
+                                                value: true,
+                                                message: 'Bitte gebe deiner VE einen Namen.',
+                                            },
+                                            maxLength: {
+                                                value: 50,
+                                                message:
+                                                    'Das Feld darf nicht mehr als 50 Buchstaben enthalten.',
+                                            },
+                                            pattern: {
+                                                value: /^[a-zA-Z0-9äöüÄÖÜß\s_*+'":&()!?-]*$/i,
+                                                message:
+                                                    'Nur folgende Sonderzeichen sind zulässig: _*+\'":&()!?-',
+                                            },
+                                        })}
                                     />
-                                    {errors && (
-                                        <div className="text-red-500 text-sm">
-                                            {errors.map((error, index) => (
-                                                <div key={index}>{error}</div>
-                                            ))}
-                                        </div>
-                                    )}
+                                    <p className="text-red-600 pt-2">{errors.name?.message}</p>
                                 </div>
                             </div>
                         </div>
                         <div className="flex justify-around w-full">
-                            <button
-                                type="button"
-                                className="items-end bg-ve-collab-orange text-white py-3 px-5 rounded-lg invisible"
-                            >
-                                Zurück
-                            </button>
-                            <button
-                                type="submit"
-                                className="items-end bg-ve-collab-orange text-white py-3 px-5 rounded-lg"
-                                onClick={(e: React.MouseEvent) =>
-                                    handleSubmit(e, '/startingWizard/generalInformation/partners')
-                                }
-                            >
-                                Weiter
-                            </button>
+                            <div>
+                                <button
+                                    type="button"
+                                    className="items-end bg-ve-collab-orange text-white py-3 px-5 rounded-lg invisible"
+                                >
+                                    Zurück
+                                </button>
+                            </div>
+                            <div>
+                                <button
+                                    type="button"
+                                    className="items-end bg-ve-collab-orange text-white py-3 px-5 rounded-lg"
+                                    onClick={() => {
+                                        validateAndRoute(
+                                            '/startingWizard/generalInformation/partners',
+                                            router.query.plannerId,
+                                            handleSubmit(onSubmit),
+                                            isValid
+                                        );
+                                    }}
+                                >
+                                    Weiter
+                                </button>
+                            </div>
                         </div>
                     </form>
                 )}
                 <SideProgressBarSection
                     progressState={sideMenuStepsProgress}
-                    handleValidation={() => {}}
-                    isValid={true}
+                    handleValidation={handleSubmit(onSubmit)}
+                    isValid={isValid}
                     sideMenuStepsData={sideMenuStepsData}
                 />
             </div>
