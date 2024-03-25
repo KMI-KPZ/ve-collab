5d2867b288f5d776cb8cd7905eb1f1b5b09d980e
Simeon Ackermann <s.ackermann@mail.de>
2024-03-25 11:53:40 +0100

    Fix eslint error

--- frontend-vecollab/src/pages/_app.tsx
+++ frontend-vecollab/src/pages/_app.tsx
@@ -137,7 +137,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
                             toggleChatWindow={toggleChatWindow}
                         >
                             <>
-                                <Auth showLoader={false} autoForward={false} children={
+                                <Auth showLoader={false} autoForward={false}>
                                     <ChatWindow
                                         socket={socket}
                                         messageEvents={messageEvents}
@@ -146,7 +146,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
                                         open={chatOpen}
                                         toggleChatWindow={toggleChatWindow}
                                     />
-                                } />
+                                </Auth>
 
                                 {Component.auth ? (
                                     <Auth>
