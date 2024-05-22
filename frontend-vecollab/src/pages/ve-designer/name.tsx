c34e24d12a5f4fd73aed746b9533cdd0e7d2bf10
Smunfr <christian.schlecht97@web.de>
2024-05-22 14:59:57 +0200

    hotfix broken partners mapping

--- frontend-vecollab/src/pages/ve-designer/name.tsx
+++ frontend-vecollab/src/pages/ve-designer/name.tsx
@@ -85,11 +85,6 @@ export default function EssentialInformation() {
                             name: ProgressState.completed,
                         },
                     },
-                    {
-                        plan_id: router.query.plannerId,
-                        field_name: 'partners',
-                        value: [author],
-                    },
                 ],
             },
             session?.accessToken
