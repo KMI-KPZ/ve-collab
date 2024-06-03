7e8e3b50a6131317f705e1371cdd943258a7d584
Simeon Ackermann <s.ackermann@mail.de>
2024-06-03 19:20:10 +0200

    Use <Wrapper /> in /participatigCourses

--- frontend-vecollab/src/pages/ve-designer/participatingCourses.tsx
+++ frontend-vecollab/src/pages/ve-designer/participatingCourses.tsx
@@ -1,22 +1,17 @@
 import WhiteBox from '@/components/Layout/WhiteBox';
-import HeadProgressBarSection from '@/components/VE-designer/HeadProgressBarSection';
-import { fetchGET, fetchPOST } from '@/lib/backend';
-import { signIn, useSession } from 'next-auth/react';
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { RxPlus } from 'react-icons/rx';
 import { useRouter } from 'next/router';
-import LoadingAnimation from '@/components/LoadingAnimation';
-import { FormProvider, SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
+import { SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
 import {
     initialSideProgressBarStates,
     ISideProgressBarStates,
     ProgressState,
 } from '@/interfaces/ve-designer/sideProgressBar';
-import { IFineStep } from '@/pages/ve-designer/step-data/[stepName]';
-import SideProgressBarWithReactHookForm from '@/components/VE-designer/SideProgressBarWithReactHookForm';
-import PopupSaveData from '@/components/VE-designer/PopupSaveData';
 import Image from 'next/image';
 import trash from '@/images/icons/ve-designer/trash.png';
+import Wrapper from '@/components/VE-designer/Wrapper';
+import { IPlan } from '@/interfaces/planner/plannerInterfaces';
 
 export interface LectureOld {
     name: string;
@@ -42,24 +37,12 @@ const areAllFormValuesEmpty = (formValues: FormValues): boolean => {
 
 Lectures.auth = true;
 export default function Lectures() {
-    const { data: session, status } = useSession();
-    const [loading, setLoading] = useState(false);
     const router = useRouter();
     const [sideMenuStepsProgress, setSideMenuStepsProgress] = useState<ISideProgressBarStates>(
         initialSideProgressBarStates
     );
-    const [steps, setSteps] = useState<IFineStep[]>([]);
-    const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
-
-    // check for session errors and trigger the login flow if necessary
-    useEffect(() => {
-        if (status !== 'loading') {
-            if (!session || session?.error === 'RefreshAccessTokenError') {
-                console.log('forced new signIn');
-                signIn('keycloak');
-            }
-        }
-    }, [session, status]);
+    const prevpage = '/ve-designer/lectures'
+    const nextpage = '/ve-designer/target-groups'
 
     const methods = useForm<FormValues>({
         mode: 'onChange',
@@ -75,33 +58,11 @@ export default function Lectures() {
         },
     });
 
-    useEffect(() => {
-        // if router or session is not yet ready, don't make an redirect decisions or requests, just wait for the next re-render
-        if (!router.isReady || status === 'loading') {
-            setLoading(true);
-            return;
+    const setPlanerData = useCallback((plan: IPlan) => {
+        if (plan.lectures.length !== 0) {
+            methods.setValue('lectures', plan.lectures);
         }
-        // router is loaded, but still no plan ID in the query --> redirect to overview because we can't do anything without an ID
-        if (!router.query.plannerId) {
-            router.push('/plans');
-            return;
-        }
-        // to minimize backend load, request the data only if session is valid (the other useEffect will handle session re-initiation)
-        if (session) {
-            fetchGET(`/planner/get?_id=${router.query.plannerId}`, session?.accessToken).then(
-                (data) => {
-                    setLoading(false);
-                    if (data.plan.lectures.length !== 0) {
-                        methods.setValue('lectures', data.plan.lectures);
-                    }
-                    if (data.plan.progress.length !== 0) {
-                        setSideMenuStepsProgress(data.plan.progress);
-                    }
-                    setSteps(data.plan.steps);
-                }
-            );
-        }
-    }, [session, status, router, methods]);
+    }, [methods]);
 
     const { fields, append, remove } = useFieldArray({
         name: 'lectures',
@@ -109,37 +70,23 @@ export default function Lectures() {
     });
 
     const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
-        if (!areAllFormValuesEmpty(data)) {
-            await fetchPOST(
-                '/planner/update_fields',
-                {
-                    update: [
-                        {
-                            plan_id: router.query.plannerId,
-                            field_name: 'lectures',
-                            value: data.lectures,
-                        },
-                        {
-                            plan_id: router.query.plannerId,
-                            field_name: 'progress',
-                            value: {
-                                ...sideMenuStepsProgress,
-                                lectures: ProgressState.completed,
-                            },
-                        },
-                    ],
-                },
-                session?.accessToken
-            );
-        }
-    };
+        if (areAllFormValuesEmpty(data)) return
 
-    const combinedSubmitRouteAndUpdate = async (data: FormValues, url: string) => {
-        onSubmit(data);
-        await router.push({
-            pathname: url,
-            query: { plannerId: router.query.plannerId },
-        });
+        return [
+            {
+                plan_id: router.query.plannerId,
+                field_name: 'lectures',
+                value: data.lectures,
+            },
+            {
+                plan_id: router.query.plannerId,
+                field_name: 'progress',
+                value: {
+                    ...sideMenuStepsProgress,
+                    lectures: ProgressState.completed,
+                },
+            },
+        ]
     };
 
     const renderLecturesInputs = (): JSX.Element[] => {
@@ -277,95 +224,39 @@ export default function Lectures() {
             </div>
         ));
     };
+
     return (
-        <FormProvider {...methods}>
-            <PopupSaveData
-                isOpen={isPopupOpen}
-                handleContinue={async () => {
-                    await router.push({
-                        pathname: '/ve-designer/targetGroups',
-                        query: {
-                            plannerId: router.query.plannerId,
-                        },
-                    });
-                }}
-                handleCancel={() => setIsPopupOpen(false)}
-            />
-            <div className="flex bg-pattern-left-blue-small bg-no-repeat">
-                <div className="flex flex-grow justify-center">
-                    <div className="flex flex-col">
-                        <HeadProgressBarSection stage={0} linkFineStep={steps[0]?.name} />
-                        {loading ? (
-                            <LoadingAnimation />
-                        ) : (
-                            <form className="gap-y-6 w-full p-12 max-w-7xl items-center flex flex-col flex-grow justify-between">
-                                <div>
-                                    <div className={'text-center font-bold text-4xl mb-2'}>
-                                        Im Rahmen welcher Lehrveranstaltungen wird der VE umgesetzt?
-                                    </div>
-                                    <div className={'text-center mb-20'}>optional</div>
-                                    <div className="flex flex-wrap justify-center">
-                                        {renderLecturesInputs()}
-                                    </div>
-                                    <div className="flex justify-center ">
-                                        <button
-                                            className="p-4 bg-white rounded-3xl shadow-2xl"
-                                            type="button"
-                                            onClick={() => {
-                                                append({
-                                                    name: '',
-                                                    lecture_type: '',
-                                                    lecture_format: '',
-                                                    participants_amount: '',
-                                                });
-                                            }}
-                                        >
-                                            <RxPlus size={30} />
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="flex justify-between w-full max-w-xl">
-                                    <div>
-                                        <button
-                                            type="button"
-                                            className="items-end bg-ve-collab-orange text-white py-3 px-5 rounded-lg"
-                                            onClick={methods.handleSubmit((data) => {
-                                                combinedSubmitRouteAndUpdate(
-                                                    data,
-                                                    '/ve-designer/lectures'
-                                                );
-                                            })}
-                                        >
-                                            Zurück
-                                        </button>
-                                    </div>
-                                    <div>
-                                        <button
-                                            type="button"
-                                            className="items-end bg-ve-collab-orange text-white py-3 px-5 rounded-lg"
-                                            onClick={methods.handleSubmit(
-                                                (data) => {
-                                                    combinedSubmitRouteAndUpdate(
-                                                        data,
-                                                        '/ve-designer/target-groups'
-                                                    );
-                                                },
-                                                async () => setIsPopupOpen(true)
-                                            )}
-                                        >
-                                            Weiter
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
-                        )}
-                    </div>
-                </div>
-                <SideProgressBarWithReactHookForm
-                    progressState={sideMenuStepsProgress}
-                    onSubmit={onSubmit}
-                />
+        <Wrapper
+            methods={methods}
+            prevpage={prevpage}
+            nextpage={nextpage}
+            setProgress={setSideMenuStepsProgress}
+            planerDataCallback={setPlanerData}
+            submitCallback={onSubmit}
+        >
+            <div className={'text-center font-bold text-4xl mb-2'}>
+                Im Rahmen welcher Lehrveranstaltungen wird der VE umgesetzt?
             </div>
-        </FormProvider>
+            <div className={'text-center mb-20'}>optional</div>
+            <div className="flex flex-wrap justify-center">
+                {renderLecturesInputs()}
+            </div>
+            <div className="flex justify-center ">
+                <button
+                    className="p-4 bg-white rounded-3xl shadow-2xl"
+                    type="button"
+                    onClick={() => {
+                        append({
+                            name: '',
+                            lecture_type: '',
+                            lecture_format: '',
+                            participants_amount: '',
+                        });
+                    }}
+                >
+                    <RxPlus size={30} />
+                </button>
+            </div>
+        </Wrapper>
     );
 }
