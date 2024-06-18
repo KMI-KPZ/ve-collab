ba2d5acebca805d5cfa8d8f70b39ae61ca164055
Smunfr <christian.schlecht97@web.de>
2024-06-18 08:55:41 +0200

    refetch session every 5 mins to avoid token invalidation

--- frontend-vecollab/src/pages/_app.tsx
+++ frontend-vecollab/src/pages/_app.tsx
@@ -131,7 +131,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
 
     return (
         <>
-            <SessionProvider session={session}>
+            <SessionProvider session={session} refetchInterval={5 * 60}>
                 <CookiesProvider defaultSetOptions={{ path: '/' }}>
                     <SocketAuthenticationProvider>
                         <Head>
