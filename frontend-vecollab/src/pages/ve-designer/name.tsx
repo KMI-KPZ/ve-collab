2410c3d340fb4fe3b150f64f0201b369bf884ae7
JonasGreim <jonasgreim@gmail.com>
2024-08-12 18:44:05 +0200

    fix null error message

--- frontend-vecollab/src/pages/ve-designer/name.tsx
+++ frontend-vecollab/src/pages/ve-designer/name.tsx
@@ -16,7 +16,8 @@ const NameFormSchema = z.object({
     name: z
         .string()
         .min(1, 'Ein Name ist erforderlich.')
-        .max(100, 'Ein gültiger Name darf maximal 100 Buchstaben lang sein.'),
+        .max(100, 'Ein gültiger Name darf maximal 100 Buchstaben lang sein.')
+        .nullable(),
 });
 
 interface FormValues {
