6af8540a72dc4ba11e254924f62166dd825bd861
JonasGreim <jonasgreim@gmail.com>
2024-09-18 11:44:15 +0200

    fix error that empty date is also possible

--- frontend-vecollab/src/pages/ve-designer/learning-env.tsx
+++ frontend-vecollab/src/pages/ve-designer/learning-env.tsx
@@ -28,29 +28,47 @@ const LearningEnvFormSchema = z.object({
             location: z
                 .string()
                 .max(400, 'Ein gültiges Thema darf maximal 400 Buchstaben lang sein.'),
-            timestamp_from: z.string().date('Bitte ein gültiges Datum eingeben'), // it is still a string
-            timestamp_to: z.string().date('Bitte ein gültiges Datum eingeben'),
+            timestamp_from: z.union([
+                z.string().date('Bitte ein gültiges Datum eingeben'),
+                z.string().max(0),
+            ]), // it is still a string
+            timestamp_to: z.union([
+                z.string().date('Bitte ein gültiges Datum eingeben'),
+                z.string().max(0),
+            ]),
         })
         .refine(
-            (data) =>
-                new Date(data.timestamp_from) >= new Date(1950, 0) &&
-                new Date(data.timestamp_from) <= new Date(2100, 0),
+            (data) => {
+                if (data.timestamp_from === '') return true;
+                return (
+                    new Date(data.timestamp_from) >= new Date(1950, 0) &&
+                    new Date(data.timestamp_from) <= new Date(2100, 0)
+                );
+            },
             {
                 message: 'Von: Bitte geben sie ein realistisches Datum an',
                 path: ['timestamp_from'], // This will attach the error message to the `timestamp_from` field
             }
         )
         .refine(
-            (data) =>
-                new Date(data.timestamp_to) >= new Date(1950, 0) &&
-                new Date(data.timestamp_to) <= new Date(2100, 0),
+            (data) => {
+                if (data.timestamp_to === '') return true;
+                return (
+                    new Date(data.timestamp_to) >= new Date(1950, 0) &&
+                    new Date(data.timestamp_to) <= new Date(2100, 0)
+                );
+            },
             {
                 message: 'Bis: Bitte geben sie ein realistisches Datum an',
                 path: ['timestamp_to'],
             }
         )
         .superRefine((data, ctx) => {
-            if (new Date(data.timestamp_from) > new Date(data.timestamp_to)) {
+            if (
+                data.timestamp_from !== '' &&
+                data.timestamp_to !== '' &&
+                new Date(data.timestamp_from) > new Date(data.timestamp_to)
+            ) {
                 ctx.addIssue({
                     code: z.ZodIssueCode.custom,
                     message: 'Das Startdatum muss an oder vor dem Enddatum liegen',
@@ -141,8 +159,12 @@ export default function Methodology({ socket }: Props): JSX.Element {
             if (plan.physical_mobilities?.length > 0) {
                 data.physicalMobilities = plan.physical_mobilities.map((physicalMobilityObject) => {
                     return Object.assign({}, physicalMobilityObject, {
-                        timestamp_from: physicalMobilityObject.timestamp_from.split('T')[0],
-                        timestamp_to: physicalMobilityObject.timestamp_to.split('T')[0],
+                        timestamp_from: physicalMobilityObject.timestamp_from
+                            ? physicalMobilityObject.timestamp_from.split('T')[0]
+                            : undefined,
+                        timestamp_to: physicalMobilityObject.timestamp_to
+                            ? physicalMobilityObject.timestamp_to.split('T')[0]
+                            : undefined,
                     });
                 });
                 replace(data.physicalMobilities);
