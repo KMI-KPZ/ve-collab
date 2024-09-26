dbc6d08e344a7dc0505305e8e388fc7c9b36a062
JonasGreim <jonasgreim@gmail.com>
2024-09-26 16:40:05 +0200

    add error message to max file count

--- frontend-vecollab/src/zod-schemas/postProcessSchema.tsx
+++ frontend-vecollab/src/zod-schemas/postProcessSchema.tsx
@@ -2,28 +2,15 @@ import { z } from 'zod';
 
 export const PostProcessSchema = z.object({
     share: z.boolean(),
-    abstract: z
-        .string()
-        .max(1000,"messages.maxlength1000")
-        .nullable(),
-    reflection: z
-        .string()
-        .max(1000,"messages.maxlength1000")
-        .nullable(),
-    veModel: z
-        .string()
-        .max(1000,"messages.maxlength1000")
-        .nullable(),
+    abstract: z.string().max(1000, 'messages.maxlength1000').nullable(),
+    reflection: z.string().max(1000, 'messages.maxlength1000').nullable(),
+    veModel: z.string().max(1000, 'messages.maxlength1000').nullable(),
     evaluation: z.string().nullable(),
     evaluationFile: z
         .object({
             file: z.any().optional(),
             file_name: z.string(),
-            size: z
-                .number()
-                .int()
-                .max(5242880, "messages.max_5_mb")
-                .optional(),
+            size: z.number().int().max(5242880, 'messages.max_5_mb').optional(),
             file_id: z.string().optional(),
         })
         .nullable(),
@@ -39,10 +26,10 @@ export const PostProcessSchema = z.object({
                 })
                 .refine((data) => data.size !== undefined && data.size <= 5242880, {
                     // 25.470.060
-                    message: "messages.max_5_mb",
+                    message: 'messages.max_5_mb',
                     path: ['size'], // This will attach the error message to the `size` field
                 })
         )
-        .max(4, 'max. 4 Dateien erlaubt')
+        .max(4, 'messages.file_count_max')
         .nullable(),
 });
