6771a252eb184fe716374f268b32e19ea6b766f9
JonasGreim <jonasgreim@gmail.com>
2024-10-29 16:15:45 +0100

    test local build

--- frontend-vecollab/lighthouserc.js
+++ frontend-vecollab/lighthouserc.js
@@ -1,8 +1,11 @@
 module.exports = {
     ci: {
         collect: {
-            url: ['http://localhost:3000/'],
+            url: ['http://localhost:3000/de'],
             startServerCommand: 'npm run start',
+            settings: {
+                preset: 'desktop',
+            },
         },
         upload: {
             target: 'temporary-public-storage',
