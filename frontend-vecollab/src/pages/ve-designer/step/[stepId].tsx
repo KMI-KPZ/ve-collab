03f19a885a7a3083e326fe4694b425a3da447b08
Simeon Ackermann <s.ackermann@mail.de>
2024-11-08 11:19:47 +0100

    fix useEffect dep

--- frontend-vecollab/src/pages/ve-designer/step/[stepId].tsx
+++ frontend-vecollab/src/pages/ve-designer/step/[stepId].tsx
@@ -182,7 +182,7 @@ export default function FinePlanner({ socket }: Props): JSX.Element {
 
             return { ...fineStepCopyTransformedTools };
         },
-        [stepId]
+        [stepId, router]
     );
 
     useEffect(() => {
