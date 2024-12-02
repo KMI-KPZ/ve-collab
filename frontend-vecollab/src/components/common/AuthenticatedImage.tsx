c09d74c6998a411e5c2b0432ac1d9694f4fc2f4e
Smunfr <christian.schlecht97@web.de>
2024-12-02 09:15:47 +0100

    catch race condition that (rarely) appears when image is loaded faster than session object

--- frontend-vecollab/src/components/common/AuthenticatedImage.tsx
+++ frontend-vecollab/src/components/common/AuthenticatedImage.tsx
@@ -17,6 +17,7 @@ export default function AuthenticatedImage({ imageId, alt, width, height, classN
     const [image, setImage] = useState('');
 
     useEffect(() => {
+        if (status === 'loading' || !session) return;
         (async () => {
             if (imageId) {
                 //Get if url already exists in local storage
@@ -30,7 +31,7 @@ export default function AuthenticatedImage({ imageId, alt, width, height, classN
                     // read the blob and create a data url from it that is readable by the Image component
                     const reader = new FileReader();
                     reader.readAsDataURL(response);
-                    reader.onerror = function() {}
+                    reader.onerror = function () {};
                     reader.onloadend = function () {
                         setImage(reader.result as string);
                         window.localStorage.setItem(imageId, reader.result as string);
@@ -38,14 +39,15 @@ export default function AuthenticatedImage({ imageId, alt, width, height, classN
                 }
             }
         })();
-    }, [session, imageId]);
+    }, [session, status, imageId]);
 
     return (
         <>
-            {image !== ''
-                ? (<Image src={image} alt={alt} width={width} height={height} className={className} />)
-                : imageId && (<div className={className} style={{height, width}}></div>)
-            }
+            {image !== '' ? (
+                <Image src={image} alt={alt} width={width} height={height} className={className} />
+            ) : (
+                imageId && <div className={className} style={{ height, width }}></div>
+            )}
         </>
     );
 }
