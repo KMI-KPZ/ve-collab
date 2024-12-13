e9fe36b3b2cc6ebe9508cf788ca71d9ee42324d9
Smunfr <christian.schlecht97@web.de>
2024-12-13 10:59:52 +0100

    hotfix: revert back to be able to set aribitrary name as partner in a plan

--- frontend-vecollab/src/pages/ve-designer/partners.tsx
+++ frontend-vecollab/src/pages/ve-designer/partners.tsx
@@ -3,7 +3,7 @@ import { useSession } from 'next-auth/react';
 import React, { useCallback, useState } from 'react';
 import { RxMinus, RxPlus } from 'react-icons/rx';
 import { useRouter } from 'next/router';
-import AsyncSelect from 'react-select/async';
+import AsyncCreatableSelect from 'react-select/async-creatable';
 import { Controller, SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
 import {
     BackendProfileSnippetsResponse,
@@ -16,7 +16,7 @@ import Wrapper from '@/components/VE-designer/Wrapper';
 import { IPlan } from '@/interfaces/planner/plannerInterfaces';
 import { Socket } from 'socket.io-client';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useTranslation } from 'next-i18next';
+import { Trans, useTranslation } from 'next-i18next';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { PartnersFormSchema } from '../../zod-schemas/partnersSchema';
 import CustomHead from '@/components/metaData/CustomHead';
@@ -286,7 +286,7 @@ export default function Partners({ socket }: Props): JSX.Element {
                 name={name}
                 control={control}
                 render={({ field: { onChange, onBlur, value } }) => (
-                    <AsyncSelect
+                    <AsyncCreatableSelect
                         className="grow max-w-full"
                         instanceId={index.toString()}
                         isClearable={true}
@@ -299,10 +299,15 @@ export default function Partners({ socket }: Props): JSX.Element {
                         placeholder={t('common:search_users')}
                         getOptionLabel={(option) => option.label}
                         autoFocus={true}
-                        loadingMessage={() => t('common:loading') + '...'}
-                        noOptionsMessage={() => t('common:nothing_found')}
-                        openMenuOnFocus={false}
-                        openMenuOnClick={false}
+                        formatCreateLabel={(inputValue) => (
+                            <span>
+                                <Trans
+                                    i18nKey="common:search_users_no_hit"
+                                    values={{ value: inputValue }}
+                                    components={{ bold: <strong /> }}
+                                />
+                            </span>
+                        )}
                         components={{
                             DropdownIndicator: null,
                         }}
