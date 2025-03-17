ab1d25679b7b91b2f29750a51e2a7eec909935e8
Smunfr <christian.schlecht97@web.de>
2025-03-17 11:37:08 +0100

    add cursor-pointer to cookie banner

--- frontend-vecollab/src/components/common/buttons/ButtonPrimary.tsx
+++ frontend-vecollab/src/components/common/buttons/ButtonPrimary.tsx
@@ -4,8 +4,8 @@ import Button, { ButtonProps } from './Button';
 export default function ButtonPrimary({ ...props }: ButtonProps) {
     const defaulStyle = `text-white ${
         props.disabled
-            ? 'bg-ve-collab-orange-light'
-            : 'bg-ve-collab-orange hover:shadow-button-primary'
+            ? 'bg-ve-collab-orange-light cursor-default'
+            : 'bg-ve-collab-orange hover:shadow-button-primary cursor-pointer'
     }`;
 
     return <Button {...props} className={`${defaulStyle} ${props.className || ''}`} />;
