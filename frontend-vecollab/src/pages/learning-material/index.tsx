d0a885a5765532dbd7ec3b2e56d541599f535eb7
Smunfr <christian.schlecht97@web.de>
2025-03-27 09:51:49 +0100

    add new bubble

--- frontend-vecollab/src/pages/learning-material/index.tsx
+++ frontend-vecollab/src/pages/learning-material/index.tsx
@@ -104,6 +104,7 @@ export default function PageCategoryNotSelected(props: Props) {
             {Bubble('Zusammen Planen', 'xl:top-[3rem] xl:left-[17%] xl:-translate-x-1/2', [
                 '-top-[2rem] -left-[5rem]', // va-planung
                 '-bottom-[1.5rem] -right-[5rem]', // evaluation
+                '-bottom-[.5rem] -left-[10rem]' // leitfragen Aushandlungsphase
             ])}
 
             {/* 3 BUBBLE */}
